dfcc1f3e5cbd9bd8726aed46b6bc820f1d3a85d1
takashicompany <t@kashi.company>
2020-07-27 12:53:46 +0900

    Venry改めUtilsに改名

--- Venry.cs
+++ Venry.cs
@@ -1,12 +1,165 @@
 ﻿namespace TakashiCompany
 {
+	using System.Collections.Generic;
 	using UnityEngine;
 
-	/// <summary>
-	/// Utilクラス。便利
-	/// </summary>
-	public static class Venry
+	public static class Utils
 	{
+		public static float GetAngleXZ(Vector3 start, Vector3 target)
+		{
+			return GetAngle(new Vector2(start.x, start.z), new Vector2(target.x, target.z));
+		}
+
+		// https://t-stove-k.hatenablog.com/entry/2018/08/28/164549
+		public static float GetAngle(Vector2 start, Vector2 target)
+		{
+			Vector2 dt = target - start;
+			float rad = Mathf.Atan2(dt.x, dt.y);
+			float degree = rad * Mathf.Rad2Deg;
+			
+			if (degree < 0) 
+			{
+				degree += 360;
+			}
+			
+			return degree;
+		}
+
+		// https://setchi.hatenablog.com/entry/2017/07/12/202756
+		public static bool LineSegmentsIntersection(
+			Vector2 p1,
+			Vector2 p2,
+			Vector2 p3,
+			Vector2 p4,
+			out Vector2 intersection)
+		{
+			intersection = Vector2.zero;
+
+			var d = (p2.x - p1.x) * (p4.y - p3.y) - (p2.y - p1.y) * (p4.x - p3.x);
+
+			if (d == 0.0f)
+			{
+				return false;
+			}
+
+			var u = ((p3.x - p1.x) * (p4.y - p3.y) - (p3.y - p1.y) * (p4.x - p3.x)) / d;
+			var v = ((p3.x - p1.x) * (p2.y - p1.y) - (p3.y - p1.y) * (p2.x - p1.x)) / d;
+
+			if (u < 0.0f || u > 1.0f || v < 0.0f || v > 1.0f)
+			{
+				return false;
+			}
+
+			intersection.x = p1.x + u * (p2.x - p1.x);
+			intersection.y = p1.y + u * (p2.y - p1.y);
+
+			return true;
+		}
+
+		public static bool LineSegmentsIntersectionXZ(
+			Vector3 p1,
+			Vector3 p2,
+			Vector3 p3,
+			Vector3 p4,
+			out Vector2 intersection)
+		{
+			return LineSegmentsIntersection(
+				new Vector2(p1.x, p1.z),
+				new Vector2(p2.x, p2.z),
+				new Vector2(p3.x, p3.z),
+				new Vector2(p4.x, p4.z),
+				out intersection
+			);
+		}
 		
+		/// <summary>
+		/// ベクトルABがあったとして、そこに点Pから下ろした垂線との交点を求めたい時に使う
+		/// https://sleepygamersmemo.blogspot.com/2019/03/perpendicular-foot-point.html
+		/// </summary>
+		/// <returns></returns>
+		public static Vector3 PerpendicularFootPoint(Vector3 a, Vector3 b, Vector3 p)
+		{
+			return a + Vector3.Project(p - a, b - a);
+		}
+
+		public static Vector3 GetPointOfProgress(this IList<Vector3> path, float progress)
+		{
+			progress = Mathf.Clamp01(progress);
+
+			var totalLength = path.GetTotalLength();
+
+			var lengthOnProgress = totalLength * progress;
+
+			var currentLength = 0f;
+
+			for (int i = 0; i < path.Count - 1; i++)
+			{
+				var head = path[i];
+				var tail = path[i + 1];
+				
+				var distance = Vector3.Distance(head, tail);
+
+				if (lengthOnProgress <= currentLength + distance)
+				{
+					var myLength = lengthOnProgress - currentLength;
+
+					var myProgress = myLength / distance;
+
+					return Vector3.Lerp(head, tail, myProgress);
+				}
+			}
+
+			return path[path.Count - 1];
+		}
+
+		public static float GetTotalLength(this IList<Vector3> path)
+		{
+			var length = 0f;
+
+			for (int i = 0; i < path.Count - 1; i++)
+			{
+				var head = path[i];
+				var tail = path[i + 1];
+
+				length += Vector3.Distance(head, tail);
+			}
+
+			return length;
+		}
+		
+		public static Vector3 GetPointOnPath(this IList<Vector3> path, Vector3 point, out float ratio)
+		{
+			var result = point;
+
+			var totalLength = 0f;
+
+			var distanceByStart = 0f;
+
+			for (int i = 0;i < path.Count - 1; i++)
+			{
+				var start = path[i];
+				var end = path[i + 1];
+
+				var p = PerpendicularFootPoint(start, end, point);
+
+				var maxDistance = Vector3.Distance(start, end);
+				var distance = Vector3.Distance(point, p);
+
+				if (i == 0 || distance < Vector3.Distance(point, result))
+				{
+					var pDistance = Vector3.Distance(start, p);
+					distanceByStart = totalLength + pDistance;
+					result = p;
+				}
+
+				totalLength += maxDistance;
+			}
+
+			ratio = distanceByStart / totalLength;
+
+			// Debug.Log("ratio:" + ratio + " d:" + distanceByStart + " tl:" + totalLength);
+
+			return result;
+		}
 	}
 }
