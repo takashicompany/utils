7ded188a2881a021d9f0486758e0a48b5eaf49c2
takashicompany <t@kashi.company>
2020-08-24 12:25:52 +0900

    ランダムで色を作る

--- Utils.cs
+++ Utils.cs
@@ -239,6 +239,14 @@
 			}
 		}
 #endregion
+
+#region 色関係
+		public static Color GetRandomColor()
+		{
+			return new Color(Random.Range(0f, 1f), Random.Range(0, 1f), Random.Range(0, 1), 1);
+		}
+#endregion
+
 	}
 
 }
