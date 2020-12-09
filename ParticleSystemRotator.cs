052619e25fffbbfe10bbcfccddb2306be2d3f178
takashicompany <t@kashi.company>
2020-12-09 20:22:14 +0900

    名前を変更

--- ParticleSystemRotator.cs
+++ ParticleSystemRotator.cs
@@ -12,25 +12,25 @@
 		private ParticleSystem _rootInternal;
 		private ParticleSystem _root => _rootInternal ?? (_rootInternal = GetComponent<ParticleSystem>());
 
-		public void Play(float angle, float range = 0)
+		public void Play(float rotation, float range = 0)
 		{
 			foreach (var ps in _targets)
 			{
 				var main = ps.main;
-				angle -= 70;	// この-70の意味が分からない...
+				rotation -= 70;	// この-70の意味が分からない...
 
 				var halfRange = range / 2;
-				var r = new ParticleSystem.MinMaxCurve((angle - halfRange) / 180f * Mathf.PI, (angle + halfRange) / 180f * Mathf.PI);
+				var r = new ParticleSystem.MinMaxCurve((rotation - halfRange) / 180f * Mathf.PI, (rotation + halfRange) / 180f * Mathf.PI);
 				main.startRotation = r;
 			}
 
 			_root.Play(true);
 		}
 
-		public void Play(Vector3 position, float angle, float range = 0)
+		public void Play(Vector3 position, float rotation, float range = 0)
 		{
 			transform.position = position;
-			Play(angle, range);
+			Play(rotation, range);
 		}
 	}
 }
