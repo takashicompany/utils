958d76c889dfe9f7ebb4df04aac8b5db1d8282c4
takashicompany <t@kashi.company>
2020-12-09 18:45:21 +0900

    Particleを動的に指定した角度で放出したい場合に使うコンポーネント

---
+++ ParticleSystemAngleController.cs
@@ -0,0 +1,36 @@
+﻿namespace TakashiCompany.Unity
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class ParticleSystemAngleController : MonoBehaviour
+	{
+		[SerializeField]
+		private ParticleSystem[] _targets;
+		
+		private ParticleSystem _rootInternal;
+		private ParticleSystem _root => _rootInternal ?? (_rootInternal = GetComponent<ParticleSystem>());
+
+		public void Play(float angle, float range = 0)
+		{
+			foreach (var ps in _targets)
+			{
+				var main = ps.main;
+				angle -= 70;	// この-70の意味が分からない...
+
+				var halfRange = range / 2;
+				var r = new ParticleSystem.MinMaxCurve((angle - halfRange) / 180f * Mathf.PI, (angle + halfRange) / 180f * Mathf.PI);
+				main.startRotation = r;
+			}
+
+			_root.Play(true);
+		}
+
+		public void Play(Vector3 position, float angle, float range = 0)
+		{
+			transform.position = position;
+			Play(angle, range);
+		}
+	}
+}
