d5e7c18f75cf772eb37c53dd4fa45d0df7645170
takashicompany <t@kashi.company>
2021-02-16 13:09:11 +0900

    MeshGeneratorを召喚するスクリプト

---
+++ mesh-generator-subtree.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+repo=git@github.com:takashicompany/mesh-generator.git
+remote=mesh-generator
+branch=master
+path=Assets/TakashiCompany/MeshGenerator
+
+if [ $# -eq 0 ]; then
+	echo "this is git-subtree-suppot tool.\noptions:\n init\n pull\n push"
+	exit 1
+fi
+
+if [ $1 = init ]; then
+	git remote add ${remote} ${repo}
+	git fetch ${remote}
+	git subtree add --prefix=${path} ${remote} ${branch}
+elif [ $1 = pull ] ; then
+	git subtree pull --prefix=${path} ${remote} ${branch}
+elif [ $1 = push ] ; then
+	git subtree push --prefix=${path} ${remote} ${branch}
+else
+	echo "$1 option not found"
+fi
