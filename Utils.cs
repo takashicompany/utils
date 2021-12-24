a84938724951e52c7675ea503b07f7151eb32ac0
takashicompany <t@kashi.company>
2021-12-24 16:24:50 +0900

    ランダムに

--- Utils.cs
+++ Utils.cs
@@ -912,6 +912,11 @@
 			return new Bounds(center, size);
 		}
 
+		public static Vector2 RandomVector2(float min, float max)
+		{
+			return (Vector2)RandomVector3(min, max);
+		}
+
 		public static Vector3 RandomVector3(float min, float max)
 		{
 			return new Vector3(Random.Range(min, max), Random.Range(min, max), Random.Range(min, max));
