dd5b750d2cb99aa3686e4e6191f9d80974d65ae7
takashicompany <t@kashi.company>
2022-01-13 19:11:32 +0900

    Placerを強化

--- RandomPlacer.cs
+++ RandomPlacer.cs
@@ -6,56 +6,91 @@ namespace takashicompany.Unity
 
 	public class RandomPlacer : MonoBehaviour
 	{
-		[SerializeField]
-		private int _min = 10;
 
-		[SerializeField]
-		private int _max = 20;
+		[System.Serializable]
+		private class Param
+		{
+			[SerializeField]
+			private int _min = 10;
+
+			[SerializeField]
+			private int _max = 20;
+
+			[SerializeField]
+			private Bounds _bounds = new Bounds(Vector3.zero, Vector3.one);
+
+			[SerializeField]
+			private Transform[] _prefabs;
+
+			[SerializeField]
+			private Transform _root;
+
+			public List<Transform> Place()
+			{
+				var placed = new List<Transform>();
+
+				var count = Random.Range(_min, _max);
+
+				for (int i = 0; i < count; i++)
+				{
+					var prefab = _prefabs[Random.Range(0, _prefabs.Length)];
+
+					var obj = Instantiate(prefab, _root);
+
+					obj.name = prefab.name + "(" + i + ")";
+					obj.transform.position = GetBounds().RandomPoint();
+
+					placed.Add(obj);
+				}
+				
+				return placed;
+			}
+
+			private Bounds GetBounds()
+			{
+				return _bounds.Transform(_root);
+			}
+
+			public void DrawGizmos()
+			{
+				if (_root == null)
+				{
+					return;
+				}
+
+				Gizmos.color = Color.red;
+			
+				var b = GetBounds();
+
+				Gizmos.DrawWireCube(b.center, b.size);
+			}
+		}
 		
 		[SerializeField]
-		private Bounds _bounds = new Bounds(Vector3.zero, Vector3.one);
-		
-		[SerializeField]
-		private Transform[] _prefabs;
-
-		[SerializeField]
-		private Transform _root;
+		private Param[] _paramList;
 
 		private List<Transform> _placed = new List<Transform>();
 
 		void Awake()
 		{
-			var count = Random.Range(_min, _max);
-
-			for (int i = 0; i < count; i++)
+			foreach (var p in _paramList)
 			{
-				var prefab = _prefabs[Random.Range(0, _prefabs.Length)];
-
-				var obj = Instantiate(prefab, _root);
-
-				obj.transform.position = GetBounds().RandomPoint();
-
-				_placed.Add(obj);
+				_placed.AddRange(p.Place());
 			}
 		}
 
-		private Bounds GetBounds()
+		// https://hwks.hatenadiary.jp/entry/2014/06/13/022747 のため
+		void Start()
 		{
-			return _bounds.Transform(_root);
-		}
+
+		} 
 
 		private void OnDrawGizmos()
 		{
-			if (_root == null)
+			foreach (var p in _paramList)
 			{
-				return;
+				p.DrawGizmos();
 			}
-
-			Gizmos.color = Color.red;
-			
-			var b = GetBounds();
-
-			Gizmos.DrawWireCube(b.center, b.size);
 		}
 	}
 }
