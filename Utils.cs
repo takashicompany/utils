dce4d6d3d354b05e0a36c54695030580c3c066da
takashicompany <t@kashi.company>
2022-02-24 18:46:24 +0900

    関数のオーバーロードを追加

--- Utils.cs
+++ Utils.cs
@@ -1192,13 +1192,37 @@
 			return wn != 0;
 		}
 
-		// public Vector3 GetDirectionFromTouch(this Camera camera, Vector2 fromScreenPoint, Vector2 toScreenPoint, params string[] layerNames)
-		// {
-		// 	var fromRay = camera.ScreenPointToRay(fromScreenPoint);
-		// 	var toRay = camera
-		// }
+		public static bool  TryGetDirectionFromTouch(
+			this Camera camera, Vector2 fromScreenPoint, Vector2 toScreenPoint, out Vector3 direction, float distance = 1000f, params string[] layerNames)
+		{
+			return TryGetDirectionFromTouch(camera, fromScreenPoint, toScreenPoint, out direction, LayerMask.GetMask(layerNames), distance);
+		}
 
+		public static bool TryGetDirectionFromTouch(
+			this Camera camera, Vector2 fromScreenPoint, Vector2 toScreenPoint, out Vector3 direction, LayerMask layerMask, float distance = 1000f)
+		{
+			var fromRay = camera.ScreenPointToRay(fromScreenPoint);
+			RaycastHit fromRayHit;
 
+			if (!Physics.Raycast(fromRay, out fromRayHit, distance, layerMask))
+			{
+				direction = Vector3.zero;
+				return false;
+			}
+
+			var toRay = camera.ScreenPointToRay(toScreenPoint);
+			RaycastHit toRayHit;
+
+			if (!Physics.Raycast(toRay, out toRayHit, distance, layerMask))
+			{
+				direction = Vector3.zero;
+				return false;
+			}
+
+			direction = (toRayHit.point - fromRayHit.point).normalized;
+
+			return true;
+		}
 
 		public static class Debug
 		{
