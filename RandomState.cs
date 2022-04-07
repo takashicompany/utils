87cd43d6c0b25c25c2b0c9f9502a58977989431d
shindo-haruka <shindo-haruka@kayac.com>
2022-04-07 17:05:13 +0900

    Merge branch 'develop' of https://github.com/kayac/bridge-world into chon-chee/art

---
+++ RandomState.cs
@@ -0,0 +1,26 @@
+namespace takashicompany.Unity
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Linq;
+	using UnityEngine;
+
+	public class RandomState : StateMachineBehaviour 
+	{
+		[SerializeField]
+		private string _toHashString = "random";
+
+		[SerializeField]
+		private int _min = 0;
+
+		[SerializeField]
+		private int _max = 3;
+
+		public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+// override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+		{
+			var hashRandom = Animator.StringToHash(_toHashString);
+			animator.SetInteger(hashRandom,  Random.Range (_min, _max));
+		}
+	}
+}
