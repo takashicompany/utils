80edb1c733f4b62336c040650e9b8706d5900833
takashicompany <t@kashi.company>
2022-08-19 14:31:18 +0900

    Enumを一部Flag的に扱いたいときに適した属性

---
+++ EnumFlag.cs
@@ -0,0 +1,30 @@
+namespace takashicompany.Unity
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Linq;
+	using UnityEngine;
+#if UNITY_EDITOR
+	using UnityEditor;
+#endif
+	
+	[System.AttributeUsage(System.AttributeTargets.Field)]
+	public class EnumFlagAttribute : PropertyAttribute
+	{
+
+	}
+
+#if UNITY_EDITOR
+	[CustomPropertyDrawer(typeof(EnumFlagAttribute))]
+	public class EnumFlagDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			var e = (Enum)(Enum.ToObject(fieldInfo.FieldType, property.intValue));
+			var f = EditorGUI.EnumFlagsField(position, label, e);
+			property.intValue = Convert.ToInt32(f);
+		}
+	}
+#endif
+}
