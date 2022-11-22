b9069b2de0b6c0aeebb64902e15b7dbca4c340fa
takashicompany <t@kashi.company>
2022-11-22 13:21:30 +0900

    関数を追加

--- Utils.cs
+++ Utils.cs
@@ -1243,6 +1243,14 @@
 			return result;
 		}
 
+		public static Rect InverseTransformRect(this Transform self, Rect rect)
+		{
+			var result = new Rect();
+			result.min = self.InverseTransformPoint(rect.min);
+			result.max = self.InverseTransformPoint(rect.max);
+			return result;
+		}
+
 #endregion
 
 #region  RectTransform
