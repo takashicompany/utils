1e28db025842e7ee3edb4e274db6873429e442f9
takashicompany <t@kashi.company>
2023-03-10 19:25:24 +0900

    Doodleのクラスを整理

--- DoodleCollider2DMaker.cs
+++ DoodleCollider2DMaker.cs
@@ -1,107 +1,7 @@
 namespace takashicompany.Unity
 {
-	using System.Collections;
-	using System.Collections.Generic;
-	using System.Linq;
 	using UnityEngine;
-	using UnityEngine.EventSystems;
-	using UnityEngine.Events;
-
-	public class DoodleManager<T> : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler where T : DoodleManager<T>.ITouchEvent, new()
-	{
-		
-		[SerializeField, Header("線のプレハブを設定する")]
-		private LineRenderer _linePrefab;
-
-		[SerializeField]
-		private Transform _lineParent;
-
-		public interface ITouchEvent
-		{
-			LineRenderer line { get; }
-			List<Vector2> points { get; }
-			void AddPoint(Vector3 point);
-			void SetLine(LineRenderer line);
-		}
-
-		[SerializeField]
-		private UnityEvent<int> _onBeginPoint;
-
-		public UnityEvent<int> onBeginPoint => _onBeginPoint;
-
-		private HashSet<T> _touched = new HashSet<T>();
-
-		private Dictionary<int, T> _touching = new Dictionary<int, T>();
-
-		[SerializeField]
-		private List<Vector2> _lastDrawPoints;
-
-		public void OnBeginDrag(PointerEventData eventData)
-		{
-			if (BeginPoint(eventData.pointerId))
-			{
-				AddPoint(eventData.pointerId, eventData.pointerPressRaycast.worldPosition);
-				AddPoint(eventData.pointerId, eventData.pointerCurrentRaycast.worldPosition);
-			}
-		}
-
-		public void OnDrag(PointerEventData eventData)
-		{
-			AddPoint(eventData.pointerId, eventData.pointerCurrentRaycast.worldPosition);
-		}
-
-		public void OnEndDrag(PointerEventData eventData)
-		{
-			AddPoint(eventData.pointerId, eventData.pointerCurrentRaycast.worldPosition);
-			EndPoint(eventData.pointerId);
-		}
-
-		public bool BeginPoint(int pointerId)
-		{
-			if (!_touching.ContainsKey(pointerId))
-			{
-				var line = Instantiate(_linePrefab, _lineParent);
-				line.positionCount = 0;
-				var touchEvent = GetTouchEvent();
-				touchEvent.SetLine(line);
-				_touching.Add(pointerId, touchEvent);
-				_onBeginPoint?.Invoke(pointerId);
-				return true;
-			}
-
-			return false;
-		}
-
-		public void AddPoint(int pointerId, Vector3 point)
-		{
-			if (_touching.TryGetValue(pointerId, out var drawnMesh))
-			{
-				drawnMesh.AddPoint(point);
-			}
-		}
-
-		public void EndPoint(int pointerId)
-		{
-			if (_touching.ContainsKey(pointerId))
-			{
-				_touched.Add(_touching[pointerId]);
-				_lastDrawPoints = _touching[pointerId].points;
-				_touching.Remove(pointerId);
-			}
-		}
-
-		public LineRenderer GetDrawingLine(int pointerId)
-		{
-			_touching.TryGetValue(pointerId, out var mesh);
-			return mesh.line;
-		}
-
-		protected virtual T GetTouchEvent()
-		{
-			return new T();
-		}
-	}
-
+	using System.Collections.Generic;
 
 	public class DoodleCollider2DMaker : DoodleManager<DoodleCollider2DMaker.DrawnMesh>
 	{
@@ -109,7 +9,7 @@ namespace takashicompany.Unity
 		private float _lineColliderEdgeRadius = 0.1f;
 
 		[System.Serializable]
-		public class DrawnMesh : ITouchEvent
+		public class DrawnMesh : DoodleManager.ITouchEvent
 		{
 			public LineRenderer line { get; private set; }
 			public EdgeCollider2D collider { get; private set; }
