058faa69c4310c02c1b63592a1abe2a4d32d632f
takashicompany <t@kashi.company>
2023-06-19 14:38:33 +0900

    pushallに対応

--- utils-subtree.sh
+++ utils-subtree.sh
@@ -4,20 +4,27 @@ repo=git@github.com:takashicompany/utils.git
 remote=utils
 branch=master
 path=Assets/takashicompany/Utils
+file_pattern="*subtree.sh"
 
 if [ $# -eq 0 ]; then
-	echo "this is git-subtree-suppot tool.\noptions:\n init\n pull\n push"
-	exit 1
+    echo "This is git-subtree-support tool.\nOptions:\n init\n pull\n push\n push_subtree_sh"
+    exit 1
 fi
 
 if [ $1 = init ]; then
-	git remote add ${remote} ${repo}
-	git fetch ${remote}
-	git subtree add --prefix=${path} ${remote} ${branch}
+    git remote add ${remote} ${repo}
+    git fetch ${remote}
+    git subtree add --prefix=${path} ${remote} ${branch}
 elif [ $1 = pull ] ; then
-	git subtree pull --prefix=${path} ${remote} ${branch}
+    git subtree pull --prefix=${path} ${remote} ${branch}
 elif [ $1 = push ] ; then
-	git subtree push --prefix=${path} ${remote} ${branch}
+    if git show-ref --quiet --verify refs/remotes/${remote}/${branch}; then
+        git subtree push --prefix=${path} ${remote} ${branch}
+    else
+        echo "Subtree is not registered. Please run 'init' option first."
+    fi
+elif [ $1 = pushall ] ; then
+    find . -maxdepth 1 -type f -name "${file_pattern}" -exec sh {} push \;
 else
-	echo "$1 option not found"
+    echo "$1 option not found"
 fi
