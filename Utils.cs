898967f08e07d7971a9213d17ad47a48d067807c
takashicompany <t@kashi.company>
2023-06-23 15:10:00 +0900

    アニメーションの再生を待機する

--- Utils.cs
+++ Utils.cs
@@ -2289,6 +2289,20 @@
 			}
 		}
 
+		public static IEnumerator CoPlayAndWait(this Animator self, string stateName, float normalizedWaitTime, System.Action onComplete = null, int layerIndex = 0)
+		{
+			self.Play(stateName, layerIndex);
+
+			yield return null;
+
+			while (self.GetCurrentAnimatorStateInfo(layerIndex).IsName(stateName) && self.GetCurrentAnimatorStateInfo(layerIndex).normalizedTime < normalizedWaitTime)
+			{
+				yield return null;
+			}
+
+			onComplete?.Invoke();
+		}
+
 		/// <summary>
 		/// 特定のStateの再生完了を待っているか。
 		/// この関数を判定に使う場合、Animator.Playを読んでから1フレーム待たないと、stateNameのstateに入らないことがある
