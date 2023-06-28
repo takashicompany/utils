f2b4722f03359e4e0dfefdf15465415f42e6c183
takashicompany <t@kashi.company>
2023-06-28 10:37:44 +0900

    ワールドで固定する

--- Tilter.cs
+++ Tilter.cs
@@ -10,15 +10,33 @@ namespace takashicompany.Unity
 		[Header("傾けたい確度")]
 		public Vector3 tiltRotate = Vector3.right * 30;
 
-		private void LateUpdate()
+		public enum RotateType
 		{
-			Tilt(transform, tiltRotate);
+			Local,
+			World
 		}
 
-		public static void Tilt(Transform transform, Vector3 tiltRotate)
+		public RotateType rotateType;
+
+		private void LateUpdate()
 		{
-			transform.localRotation = Quaternion.identity;
-			transform.Rotate(tiltRotate, Space.World);
+			Tilt(transform, rotateType, tiltRotate);
+		}
+
+		public static void Tilt(Transform transform, RotateType rotateType, Vector3 tiltRotate)
+		{
+			switch (rotateType)
+			{
+				case RotateType.Local:
+					transform.localRotation = Quaternion.identity;
+					transform.Rotate(tiltRotate, Space.World);
+					break;
+
+				case RotateType.World:
+					transform.rotation = Quaternion.Euler(tiltRotate);
+					break;
+
+			}
 		}
 	}
 }
