89bd01450f8c089bdaae93d22c33c1969f112999
takashicompany <t@kashi.company>
2023-09-12 01:20:02 +0900

    修正

--- FittableTextAreaAttribute.cs
+++ FittableTextAreaAttribute.cs
@@ -4,13 +4,30 @@ namespace takashicompany.Unity
 	using System.Collections.Generic;
 	using System.Linq;
 	using UnityEngine;
-	
-# if UNITY_EDITOR
+
+#if UNITY_EDITOR
 	using UnityEditor;
 #endif
 	public class FittableTextAreaAttribute : PropertyAttribute
 	{
-		
+		public string columnSeparate;
+		public string rowSeparete;
+
+		public FittableTextAreaAttribute()
+		{
+
+		}
+
+		public FittableTextAreaAttribute(string columnSeparate)
+		{
+			this.columnSeparate = columnSeparate;
+		}
+
+		public FittableTextAreaAttribute(string columnSeparate, string rowSeparete) : this(columnSeparate)
+		{
+			this.rowSeparete = rowSeparete;
+		}
+
 	}
 
 #if UNITY_EDITOR
@@ -21,16 +38,41 @@ namespace takashicompany.Unity
 		{
 			if (property.propertyType == SerializedPropertyType.String)
 			{
+				var attribute = (FittableTextAreaAttribute)base.attribute;
+
 				EditorGUI.BeginProperty(position, label, property);
 
 				// Calculate the height required for the text area based on the content
 				GUIContent content = new GUIContent(property.stringValue);
 				float textHeight = GUI.skin.textArea.CalcHeight(content, position.width);
 
-				// Create a rect for the text area
-				Rect textAreaRect = new Rect(position.x, position.y, position.width, textHeight);
+				// Create a rect for the label above the text area
+				Rect labelRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
 
-				// Draw the text area
+				// Create a rect for the text area below the label
+				Rect textAreaRect = new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight, position.width, textHeight);
+
+				var newLabel = new GUIContent(label);
+				// newLabel.text += " Length: " + property.stringValue.Length.ToString() + " ";
+
+				if (!string.IsNullOrEmpty(attribute.columnSeparate))
+				{
+					string[,] map;
+
+					if (!string.IsNullOrEmpty(attribute.rowSeparete))
+					{
+						map = property.stringValue.Get2dMap(attribute.columnSeparate, attribute.rowSeparete);
+					}
+					else
+					{
+						map = property.stringValue.Get2dMap(attribute.columnSeparate);
+					}
+
+					newLabel.text += "(" + map.GetLength(0) + ", " + map.GetLength(1) + ")";
+				}
+
+				// Draw the label and text area
+				EditorGUI.LabelField(labelRect, newLabel);
 				property.stringValue = EditorGUI.TextArea(textAreaRect, property.stringValue);
 
 				EditorGUI.EndProperty();
@@ -47,7 +89,7 @@ namespace takashicompany.Unity
 			{
 				GUIContent content = new GUIContent(property.stringValue);
 				float textHeight = GUI.skin.textArea.CalcHeight(content, EditorGUIUtility.currentViewWidth);
-				return textHeight + EditorGUIUtility.standardVerticalSpacing;
+				return textHeight + EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
 			}
 			else
 			{
