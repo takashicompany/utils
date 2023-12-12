0fc3c830a945bab3138a13c2530e32ba6dc4ff9d
takashicompany <t@kashi.company>
2023-12-12 12:56:58 +0900

    多段回転

--- OrbitController.cs
+++ OrbitController.cs
@@ -15,6 +15,15 @@ namespace takashicompany.Unity
 		}
 
 		[SerializeField]
+		private float _widthPerOrbit = 1f;
+
+		[SerializeField]
+		private float _minOrbitDistance = 2f;
+
+		[SerializeField]
+		private float _distancePerDepth = 1.25f;
+
+		[SerializeField]
 		private Axis _rotateAxis = Axis.Y;
 
 		[SerializeField, EnumFlag]
@@ -23,40 +32,78 @@ namespace takashicompany.Unity
 		[SerializeField]
 		private float _rotationPerSecond = 360f;
 
+		[SerializeField]
+		private bool _isRotateReverseForDepth = true;
+
+		private List<Transform> _rotators = new List<Transform>();
 		private List<Transform> _orbits = new List<Transform>();
 		private Dictionary<Transform, Transform> _orbitAndWrappers = new Dictionary<Transform, Transform>();
 
+		private void OnDestroy()
+		{
+			foreach (var wrapper in _orbitAndWrappers.Values)
+			{
+				Destroy(wrapper.gameObject);
+			}
+
+			foreach (var rotator in _rotators)
+			{
+				Destroy(rotator.gameObject);
+			}
+		}
+
 		private void Update()
 		{
-			if (_updateType.HasFlag(UpdateType.Update))Orbit();
+			if (_updateType.HasFlag(UpdateType.Update)) Rotate();
 		}
 
 		private void LateUpdate()
 		{
-			if (_updateType.HasFlag(UpdateType.LateUpdate))Orbit();
+			if (_updateType.HasFlag(UpdateType.LateUpdate)) Rotate();
 		}
 
 		private void FixedUpdate()
 		{
-			if (_updateType.HasFlag(UpdateType.FixedUpdate))Orbit();
+			if (_updateType.HasFlag(UpdateType.FixedUpdate)) Rotate();
 		}
 
-		private void Orbit()
+		private void Rotate()
 		{
-			foreach (var orbit in _orbits)
-			{
+			// foreach (var orbit in _orbits)
+			// {
 
-				if (!_orbitAndWrappers.TryGetValue(orbit, out var wrapper)) continue;
+			// 	if (!_orbitAndWrappers.TryGetValue(orbit, out var wrapper)) continue;
 				
+			// 	var v = Vector3.zero;
+			// 	v[(int)_rotateAxis] = _rotationPerSecond * Time.deltaTime;
+			// 	wrapper.Rotate(v);
+
+			// 	wrapper.position = transform.position;
+			// }
+
+			for (int i = 0; i < _rotators.Count; i++)
+			{
+				var rotator = _rotators[i];
 				var v = Vector3.zero;
 				v[(int)_rotateAxis] = _rotationPerSecond * Time.deltaTime;
-				wrapper.Rotate(v);
-
-				wrapper.position = transform.position;
+				rotator.Rotate(_isRotateReverseForDepth && i % 2 == 1 ? -v : v);
+				rotator.position = transform.position;
 			}
 		}
 
-		public void AddOrbit(Transform orbit, float distance)
+		private Transform GetRotator(int index)
+		{
+			while (_rotators.Count <= index)
+			{
+				var rotator = new GameObject(name + " Rotator:" + index).transform;
+				rotator.SetParent(transform.parent);
+				_rotators.Add(rotator);
+			}
+
+			return _rotators[index];
+		}
+
+		public void AddOrbit(Transform orbit)
 		{
 			if (_orbitAndWrappers.ContainsKey(orbit)) return;
 			
@@ -64,7 +111,6 @@ namespace takashicompany.Unity
 			var wrapper = new GameObject("OrbitWrapper:" + orbit.name).transform;
 			wrapper.SetParent(transform.parent);
 			orbit.SetParent(wrapper);
-			orbit.localPosition = Vector3.forward * distance;
 			_orbitAndWrappers.Add(orbit, wrapper);
 
 			Align();
@@ -83,17 +129,45 @@ namespace takashicompany.Unity
 
 		public void Align()
 		{
-			var anglePerOne = 360f / _orbits.Count;
+			var depth = 0;
 
-			for (int i = 0; i < _orbits.Count; i++)
+			int index = 0;
+
+			while (index < _orbits.Count)
 			{
-				var orbit = _orbits[i];
-				var wrapper = _orbitAndWrappers[orbit];
-				var v = Vector3.zero;
-				v[(int)_rotateAxis] = anglePerOne * i;
-				wrapper.transform.position = transform.position;
-				wrapper.localRotation = Quaternion.Euler(v);
+				var 円周 = Mathf.PI * 2f * (_minOrbitDistance + _widthPerOrbit * depth);
+				var count = (int)(円周 / _widthPerOrbit) - 1;
+				var anglePerOne = 360f / Mathf.Min(count, _orbits.Count - index);
+
+				var rotator = GetRotator(depth);
+
+				for (int i = index; i < Mathf.Min(index + count, _orbits.Count); i++)
+				{
+					var orbit = _orbits[i];
+					var wrapper = _orbitAndWrappers[orbit];
+					wrapper.SetParent(rotator);
+					var v = Vector3.zero;
+					v[(int)_rotateAxis] = anglePerOne * i;
+					wrapper.localPosition = Vector3.zero;
+					wrapper.localRotation = Quaternion.Euler(v);
+					orbit.localPosition = Vector3.forward * (_minOrbitDistance + _widthPerOrbit * depth);
+				}
+
+				index += count;
+				depth++;
 			}
+
+			// var anglePerOne = 360f / _orbits.Count;
+
+			// for (int i = 0; i < _orbits.Count; i++)
+			// {
+			// 	var orbit = _orbits[i];
+			// 	var wrapper = _orbitAndWrappers[orbit];
+			// 	var v = Vector3.zero;
+			// 	v[(int)_rotateAxis] = anglePerOne * i;
+			// 	wrapper.transform.position = transform.position;
+			// 	wrapper.localRotation = Quaternion.Euler(v);
+			// }
 		}
 	}
 }
