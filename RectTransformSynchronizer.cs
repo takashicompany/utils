3231309792bdf5ef58c4937a9b435e1b8e4abf9d
takashicompany <t@kashi.company>
2023-12-26 10:58:25 +0900

    移動させつつ、それぞれの同期をフラグにした

---
+++ RectTransformSynchronizer.cs
@@ -0,0 +1,66 @@
+namespace takashicompany.Unity
+{
+	using UnityEngine;
+
+	[ExecuteAlways]
+	[RequireComponent(typeof(RectTransform))]
+	public class RectTransformSynchronizer : MonoBehaviour
+	{
+		[SerializeField]
+		private RectTransform _target;
+
+		[SerializeField, EnumFlag]
+		private UpdateType _updateType = UpdateType.Update;
+
+		[SerializeField]
+		private bool _syncAnchoredPosition = true;
+
+		[SerializeField]
+		private bool _syncSizeDelta = true;
+
+		[SerializeField]
+		private bool _syncAnchorMin = true;
+
+		[SerializeField]
+		private bool _syncAnchorMax = true;
+
+		[SerializeField]
+		private bool _syncPivot = true;
+
+		private RectTransform _rectTransform;
+
+		private void Start()
+		{
+			// このスクリプトがアタッチされているオブジェクトのRectTransformを取得
+			_rectTransform = GetComponent<RectTransform>();
+		}
+
+		private void Update()
+		{
+			if (_updateType.IsUpdate()) Execute();
+		}
+
+		private void LateUpdate()
+		{
+			if (_updateType.IsLateUpdate()) Execute();
+		}
+
+		private void FixedUpdate()
+		{
+			if (_updateType.IsFixedUpdate()) Execute();
+		}
+
+		public void Execute()
+		{
+			if (_target != null && _rectTransform != null)
+			{
+				// 参照元のRectTransformからサイズと位置をコピー
+				if (_syncAnchoredPosition) _rectTransform.anchoredPosition = _target.anchoredPosition;
+				if (_syncSizeDelta) _rectTransform.sizeDelta = _target.sizeDelta;
+				if (_syncAnchorMin) _rectTransform.anchorMin = _target.anchorMin;
+				if (_syncAnchorMax) _rectTransform.anchorMax = _target.anchorMax;
+				if (_syncPivot) _rectTransform.pivot = _target.pivot;
+			}
+		}
+	}
+}
