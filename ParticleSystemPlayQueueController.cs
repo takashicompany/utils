284b95120d58e4c26ead3e4d76ba25f5db1f2779
takashicompany <t@kashi.company>
2023-12-28 15:44:04 +0900

    色も変えられるけど

--- ParticleSystemPlayQueueController.cs
+++ ParticleSystemPlayQueueController.cs
@@ -31,9 +31,19 @@
 		[SerializeField]
 		private ParticleSystem _particleSystem;
 
-		private ParticleSystem _p;
+		private ParticleSystem _main;
 
-		private Queue<Vector3> _positionQueue = new Queue<Vector3>();
+		private ParticleSystem[] _particles;
+
+		private Queue<Param> _paramQueue = new Queue<Param>();
+
+		private Dictionary<ParticleSystem, Color> _defaultColors = new Dictionary<ParticleSystem, Color>();
+
+		private struct Param
+		{
+			public Vector3 position;
+			public Color? color;
+		}
 
 		public void Init(Transform transform, ParticleSystem overrideParticle = null)
 		{
@@ -46,11 +56,18 @@
 			{
 				if (_particleSystem.gameObject.IsPrefab())
 				{
-					_p = GameObject.Instantiate(_particleSystem, transform);
+					_main = GameObject.Instantiate(_particleSystem, transform);
 				}
 				else
 				{
-					_p = _particleSystem;
+					_main = _particleSystem;
+				}
+
+				_particles = _main.GetComponentsInChildren<ParticleSystem>();
+
+				foreach (var p in _particles)
+				{
+					_defaultColors.Add(p, p.main.startColor.color);
 				}
 			}
 		}
@@ -58,17 +75,43 @@
 
 		public void Play(Vector3 position)
 		{
-			if (_p != null) _positionQueue.Enqueue(position);
+			if (_main != null) _paramQueue.Enqueue(new Param { position = position });
+		}
+
+		public void Play(Vector3 position, Color color)
+		{
+			if (_main != null)
+			{
+				_paramQueue.Enqueue(new Param { position = position, color = color });
+			}
 		}
 
 		public void Update()
 		{
-			if (_p != null && _positionQueue.Count > 0)
+			if (_main != null && _paramQueue.Count > 0)
 			{
-				var p = _positionQueue.Dequeue();
+				var p = _paramQueue.Dequeue();
 
-				_p.transform.position = p;
-				_p.Play(true);
+				_main.transform.position = p.position;
+
+				if (p.color.HasValue)
+				{
+					foreach (var ps in _particles)
+					{
+						var main = ps.main;
+						main.startColor = p.color.Value;
+					}
+				}
+				else
+				{
+					foreach (var ps in _particles)
+					{
+						var main = ps.main;
+						main.startColor = _defaultColors[ps];
+					}
+				}
+
+				_main.Play(true);
 			}
 		}
 	}
