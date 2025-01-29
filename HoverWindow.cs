bbe625113d38181c9498bc93272c45fa76ffb171
takashicompany <t@kashi.company>
2025-01-29 10:59:09 +0900

    HoverWindowを作成

---
+++ HoverWindow.cs
@@ -0,0 +1,12 @@
+namespace takashicompany.Unity.UI
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Linq;
+	using UnityEngine;
+
+	public class HoverWindow : MonoBehaviour
+	{
+		
+	}
+}
