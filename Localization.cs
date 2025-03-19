f4aee65722f41e59a69173f4903335982320637e
takashicompany <t@kashi.company>
2025-03-19 11:47:48 +0900

    言語変更

--- Localization.cs
+++ Localization.cs
@@ -17,6 +17,8 @@ namespace takashicompany.Unity
 		private Dictionary<string, int> _keyIndex = new Dictionary<string, int>();
 		public IEnumerable<string> keys => _keyIndex.Keys;
 
+		private const string _prefsKey = "TC_Localization_Language";
+
 		public Localization(TextAsset csvFile) : base(csvFile)
 		{
 			var langs = _raw[0].ToArray();
@@ -34,10 +36,10 @@ namespace takashicompany.Unity
 				_keyIndex[key] = i;
 			}
 		}
-		
+
 		public string Get(string key, Language lang)
 		{
-			return Get(key, ((int)lang)+ 1);	// 1列目はキーなので+1
+			return Get(key, ((int)lang) + 1);   // 1列目はキーなので+1
 		}
 
 		public string Get(string key, string lang)
@@ -47,7 +49,7 @@ namespace takashicompany.Unity
 				Debug.LogError("言語が見つかりません。" + lang);
 				return $"Lang:{lang} not found.";
 			}
-			
+
 			var langIndex = _langIndex[lang];
 
 			return Get(key, langIndex);
@@ -68,5 +70,48 @@ namespace takashicompany.Unity
 		{
 			return _keyIndex.ContainsKey(key);
 		}
+
+		public static Language GetLanguage()
+		{
+			var lang = PlayerPrefs.GetInt(_prefsKey, (int)Language.English);
+			return (Language)lang;
+		}
+
+		public static void SetLanguage(Language lang)
+		{
+			PlayerPrefs.SetInt(_prefsKey, (int)lang);
+		}
+
+#if UNITY_EDITOR
+		private const string menuPath = "翻訳/言語を切り替え/";
+		private const string menuPathJapanese = menuPath + "日本語";
+		private const string menuPathEnglish = menuPath + "英語";
+
+		[UnityEditor.MenuItem(menuPathJapanese, true)]
+		private static bool JapaneseValidate()
+		{
+			UnityEditor.Menu.SetChecked(menuPathJapanese, GetLanguage() == Language.Japanese);
+			return true;
+		}
+
+		[UnityEditor.MenuItem(menuPathJapanese, false, 1)]
+		private static void Japanese()
+		{
+			SetLanguage(Language.Japanese);
+		}
+
+		[UnityEditor.MenuItem(menuPathEnglish, true)]
+		private static bool EnglishValidate()
+		{
+			UnityEditor.Menu.SetChecked(menuPathEnglish, GetLanguage() == Language.English);
+			return true;
+		}
+
+		[UnityEditor.MenuItem(menuPathEnglish, false, 2)]
+		private static void English()
+		{
+			SetLanguage(Language.English);
+		}
+#endif
 	}
 }
