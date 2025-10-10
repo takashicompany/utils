a61f597d0fd7e82fe4573d77acc6f0300f739914
takashicompany <t@kashi.company>
2025-10-10 14:59:50 +0900

    PointerEnterも使えるようした

--- PointerScaleAnimation.cs
+++ PointerScaleAnimation.cs
@@ -7,8 +7,14 @@ namespace takashicompany.Unity
 	using UnityEngine.EventSystems;
 	using DG.Tweening;
 
-	public class PointerScaleAnimation : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+	public class PointerScaleAnimation : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
 	{
+		[SerializeField]
+		private bool _pointerEnterEnable = false;
+
+		[SerializeField]
+		private Vector3 _pointerEnterScale = new Vector3(1.1f, 1.1f, 1.1f);
+
 		[SerializeField]
 		private Vector3 _pointerDownScale = new Vector3(1.25f, 1.25f, 1.25f);
 
@@ -40,6 +46,21 @@ namespace takashicompany.Unity
 
 		void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
 		{
+			Utils.Kill(ref _tween);
+			if (!enabled) return;
+			_tween = transform.DOScale(_defaultScale, _animationDuration).SetEase(_easeType);
+		}
+
+		void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+		{
+			if (!enabled || !_pointerEnterEnable) return;
+			Utils.Kill(ref _tween);
+			transform.localScale = _pointerEnterScale;
+		}
+
+		void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+		{
+			if (!_pointerEnterEnable) return;
 			Utils.Kill(ref _tween);
 			if (!enabled) return;
 			_tween = transform.DOScale(_defaultScale, _animationDuration).SetEase(_easeType);
