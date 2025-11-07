dd33a7d940d45ba84ff83b3e3958017d8198f330
takashicompany <t@kashi.company>
2025-11-07 17:22:49 +0900

    TryGetComponentInChildren

--- Utils.cs
+++ Utils.cs
@@ -3118,6 +3118,20 @@
 			return component != null;
 		}
 
+		public static bool TryGetComponentInChildren<T>(this Component self, out T component, bool includeInactive = false)
+		{
+			component = self.GetComponentInChildren<T>(includeInactive);
+
+			return component != null;
+		}
+
+		public static bool TryGetComponentInChildren<T>(this GameObject self, out T component, bool includeInactive = false)
+		{
+			component = self.GetComponentInChildren<T>(includeInactive);
+
+			return component != null;
+		}
+
 		public static IEnumerable<A> GetComponents<A, B>(this IEnumerable<B> self) where A : Component where B : IGameObject
 		{
 			return self.Select(c => c.gameObject.GetComponent<A>()).Where(c => c != null);
