2f64c1423b1d696c322ee46abdc7aa2e23350243
takashicompany <t@kashi.company>
2025-12-16 11:13:17 +0900

    Updateのタイミングを制御できる抽象コンポーネント

---
+++ UpdateTypeSelector.cs
@@ -0,0 +1,44 @@
+namespace takashicompany.Unity
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Linq;
+	using UnityEngine;
+
+	public abstract class UpdateTypeSelector : MonoBehaviour
+	{
+		[SerializeField]
+		protected UpdateType _updateType = UpdateType.Update;
+		
+
+		protected void Update()
+		{
+			if (_updateType.IsUpdate()) OnUpdate();
+		}
+
+		protected void LateUpdate()
+		{
+			if (_updateType.IsLateUpdate()) OnLateUpdate();
+		}
+
+		protected void FixedUpdate()
+		{
+			if (_updateType.IsFixedUpdate()) OnFixedUpdate();
+		}
+
+		protected virtual void OnUpdate()
+		{
+			
+		}
+
+		protected virtual void OnLateUpdate()
+		{
+
+		}
+
+		protected virtual void OnFixedUpdate()
+		{
+
+		}
+	}
+}
